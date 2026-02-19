18f74926ece73dffb04fab47a350f6a7f59a38d2
Astha-sys <asthasingh5445@gmail.com>
2026-02-20 00:44:30 +0530

    feat: implement Redis sliding window rate limiting for API protection

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const authController = require('../controllers/auth.controller');
+const express = require("express");
+const authController = require("../controllers/auth.controller");
 const authMiddleware = require("../middleware/auth.middleware");
-
+const rateLimiter = require("../middleware/rateLimit.middleware");
 
 const router = express.Router();
 
+// Rate Limiters
+const registerLimiter = rateLimiter(60, 15); // 10 requests per minute
+const loginLimiter = rateLimiter(60, 15);    // 10 login attempts per minute
 
 
 
+router.post("/register", registerLimiter, authController.userRegisterController);
+router.post("/login", loginLimiter, authController.userLoginController);
+router.post("/logout",authMiddleware.authMiddleware,authController.userLogoutController);
 
-router.post('/register',authController.userRegisterController)
-router.post('/login',authController.userLoginController)
-router.post('/logout', authMiddleware.authMiddleware ,authController.userLogoutController)
-
-
-module.exports=router;
+module.exports = router;
